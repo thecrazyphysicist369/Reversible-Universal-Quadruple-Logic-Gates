39f29d2f4fa4789e3e51d24ab4f3497fc7eaeea7
Shaun Ray <shaun.ray.3000@gmail.com>
2021-01-31 23:27:57 +0530

    Added the QVL AND gate.
    QVL stands for quad value logic

---
+++ QVL/QVL AND.py
@@ -0,0 +1,20 @@
+import blocklogic
+print("This is simulating the QUAD valued OR gate\n")
+print("Enter the following values : \n\n")
+
+y='y'
+while y == 'y':
+    A1 = int(input("Enter A1 : "))
+    A2 = int(input("Enter A2 : "))
+    B1 = int(input("Enter B1 : "))
+    B2 = int(input("Enter B2 : "))
+    L=3
+    A0=blocklogic.blockpos(L,A1,A2)
+    B0=blocklogic.blockpos(A0,B1,B2)
+
+
+
+    print('\n\n\n')
+    C1, C2 = blocklogic.beamsplitter(B0)
+    print(A1, A2, ' AND ', B1, B2, ' = ', C1, C2)
+    y = input('\n\nto continue press y   \n')
